f33c86645e62080220873631c76099ec16f2432d
fressh1127 <fressh1127@gmail.com>
2023-08-17 15:45:08 +0900

    역네개

--- subways/views.py
+++ subways/views.py
@@ -1,5 +1,3 @@
-# subways/views.py
-
 import requests
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -7,26 +5,48 @@ from django.conf import settings
 from rest_framework import status
 from .models import Subway
 from .serializers import SubwaySerializer
-
+from .subs import Normal, nametonum
+from collections import OrderedDict
 
 REALTIME_API_KEY = settings.REALTIME_API_KEY
 
 sbw = {
-    '7호선': [1007000760, 1007000761],
-    '4호선': [1004000432,1004000433]
+    '7호선': ["산곡", "석남"],
+    '4호선': ["고잔", "중앙"],
+    '신분당선': ["광교"]
 }
 
 start_index = 0
-end_index = 10
+end_index = 50
 
 class SubwayListView(APIView):
     def get(self, request, *args, **kwargs):
 
+        result_data = {}
+
+        for subway_nm, station_names in sbw.items():
+            first_station = station_names[0]  # 첫번째 역을 선택
+            if len(station_names)>=2 :
+                second_station = station_names[-1]
+            if first_station in Normal.get(subway_nm, []):
+                idx = Normal[subway_nm].index(first_station)
+
+                if idx>=1 and Normal[subway_nm][idx-1]== second_station:
+                    idx = Normal[subway_nm].index(second_station)
+
+                adjacent_indices = range(idx - 1, idx + 3)
+                adjacent_stations = [Normal[subway_nm][i] for i in adjacent_indices if 0 <= i < len(Normal[subway_nm])]
         
-        result_data = []
+        # 인접한 역의 개수가 4개가 아니면 마지막 4개 역을 선택
+                if len(adjacent_stations) != 4:
+                    adjacent_stations = Normal[subway_nm][-4:]
+        
+                result_data[subway_nm] = {
+                    "station_list": adjacent_stations,
+                    "trains": []
+                }
 
-        for subway_nm, selected_statn_ids in sbw.items():
-
+            
             url = f"http://swopenAPI.seoul.go.kr/api/subway/{REALTIME_API_KEY}/json/realtimePosition/0/10/{subway_nm}"
             response = requests.get(url)
             data = response.json()
@@ -34,36 +54,16 @@ class SubwayListView(APIView):
                 realtime_position_list = data['realtimePositionList']
 
                 for entry in realtime_position_list:
-                    if entry['updnLine'] == '0':
-                        for i in range(-1, 3):
-                                adjacent_statn_id = selected_statn_ids[0] + i
-                                if entry['statnId'] == str(adjacent_statn_id):
-                                    result_data.append({
-                                        "line_num": entry["subwayNm"],
-                                        "direction": "상행",
-                                        "express": entry["directAt"],
-                                        "arrival_message": entry["trainSttus"],
-                                        "cur_station": entry["statnNm"],
-                                        "endstation": entry["statnTnm"],
-                                        "msg_time": entry["recptnDt"]
-                                    })
-                    elif entry['updnLine'] == '1':
-                        # Check for adjacent statnIds
-                        for i in range(-1, 3):
-                                adjacent_statn_id = selected_statn_ids[0] + i
-                                if entry['statnId'] == str(adjacent_statn_id):
-                                    result_data.append({
-                                        "line_num": entry["subwayNm"],
-                                        "direction": "하행",
-                                        "express": entry["directAt"],
-                                        "arrival_message": entry["trainSttus"],
-                                        "cur_station": entry["statnNm"],
-                                        "endstation": entry["statnTnm"],
-                                        "msg_time": entry["recptnDt"]
-                                    })
-
-
-                    
-            
+                    if entry['statnNm'] in result_data[subway_nm]["station_list"]:
+                        direction = "상행" if entry['updnLine'] == '0' else "하행"
+                        result_data[subway_nm]["trains"].append({
+                            "line_num": entry["subwayNm"],
+                            "direction": direction,
+                            "express": entry["directAt"],
+                            "arrival_message": entry["trainSttus"],
+                            "cur_station": entry["statnNm"],
+                            "endstation": entry["statnTnm"],
+                            "msg_time": entry["recptnDt"]
+                        })
 
         return Response({"result_data": result_data})
